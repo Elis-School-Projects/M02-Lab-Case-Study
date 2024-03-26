be99f128ad290eee8fd4f5e7e6cd0e6646f219ac
weirdG22 <walterssbox@gmail.com>
2024-03-25 22:46:24 -0400

    initial commit

---
+++ M02-Case-Study.py
@@ -0,0 +1,18 @@
+# Name: Eli Walters
+# File Name: M02-Case-Study.py
+# Description: This application will accept student names and GPAs and test 
+# if the student qualifies for either the Dean's List or the Honor Roll
+
+while True:
+    lname = str(input("What is the student's last name? (ZZZ to quit): "))
+
+    if lname == "ZZZ":
+        break
+
+    fname = str(input("What is the student's first name?: "))
+    gpa = float(input("What is the student's GPA?: "))
+
+    if gpa >= 3.5:
+        print(f"{fname} {lname} has made the Dean's List!")
+    elif gpa >= 3.25:
+        print(f"{fname} {lname} has made the Honor Roll!")
